b8296a5999b890963cd91754b90580a1693b6f9f
tsauter <tsauter@gmx.net>
2016-03-11 16:54:07 +0100

    Import initial code.

---
+++ NodeExporterWindows/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+using Prometheus;
+using PrometheusCollector;
+
+namespace NodeExporterWindows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter = Metrics.CreateCounter("myCounter", "some help about this");
+            counter.Inc(5.5);
+
+            string dllDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string[] pluginFiles = Directory.GetFiles(dllDirectory, "PrometheusCollector.*.dll");
+
+            foreach (string filename in pluginFiles)
+            {
+                IPrometheusCollector plugin = Program.LoadAssembly(filename);
+                plugin.RegisterMetrics();
+            }
+
+            // netsh http add urlacl url="http://+:9100/" user=everyone OR user=domain\xx
+            while (true)
+            {
+                try
+                {
+                    MetricServer metricServer = new MetricServer(port: 9100);
+                    metricServer.Start();
+                    break;
+                }
+                catch
+                {
+                    Thread.Sleep(TimeSpan.FromSeconds(10));
+                    continue;
+                }
+            }
+
+            Console.WriteLine("Waiting...");
+            Console.ReadLine();
+            Console.WriteLine("Exiting...");
+        }
+
+        private static IPrometheusCollector LoadAssembly(string assemblyPath)
+        {
+            string assembly = Path.GetFullPath(assemblyPath);
+            Assembly ptrAssembly = Assembly.LoadFile(assembly);
+            foreach (Type item in ptrAssembly.GetTypes())
+            {
+                if (!item.IsClass) continue;
+                if (item.GetInterfaces().Contains(typeof(IPrometheusCollector)))
+                {
+                    return (IPrometheusCollector)Activator.CreateInstance(item);
+                }
+            }
+            throw new Exception("Invalid DLL, Interface not found!");
+        }
+
+    }
+}
---
+++ PrometheusCollector.WindowsCore/WindowsCore.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Diagnostics;
+using PrometheusCollector;
+using Prometheus;
+
+namespace PrometheusCollector.WindowsCore
+{
+
+    public class CounterEntry
+    {
+        public System.Diagnostics.PerformanceCounter PerfCounter { get; set; }
+
+        public object PrometheusCollector { get; set; }
+    }
+
+    public class WindowsCore : IPrometheusCollector
+    {
+        private List<CounterEntry> RegisteredCounts;
+
+        public WindowsCore()
+        {
+            this.RegisteredCounts = new List<CounterEntry>();
+        }
+
+        public void RegisterMetrics()
+        {
+            /*
+             * CPU
+             */
+            string categoryName = "Processor";
+            PerformanceCounterCategory perfCategory = new PerformanceCounterCategory(categoryName);
+            string[] instanceNames = perfCategory.GetInstanceNames().Where(name => name != "_Total").ToArray();
+            foreach (string instanceName in instanceNames)
+            {
+                System.Diagnostics.PerformanceCounter tmpPerfCounter = new System.Diagnostics.PerformanceCounter();
+                tmpPerfCounter.CategoryName = categoryName;
+                tmpPerfCounter.CounterName = "% Processor Time";
+                tmpPerfCounter.InstanceName = instanceName;
+                Prometheus.Gauge tmpPrometheusCounter = Metrics.CreateGauge("perfmon_processor_processortime_percent", "help text", labelNames: new[] { "name" });
+                CounterEntry entry = new CounterEntry() { PerfCounter = tmpPerfCounter, PrometheusCollector = tmpPrometheusCounter };
+                this.RegisteredCounts.Add(entry);
+            }
+
+            this.RegisterPrintSpooler();
+
+            System.Timers.Timer metricUpdateTimer = new System.Timers.Timer(1000);
+            metricUpdateTimer.Elapsed += UpdateMetrics;
+            metricUpdateTimer.Enabled = true;
+            metricUpdateTimer.Start();
+
+            /*
+            while(true)
+            {
+                if (!metricUpdateTimer.Enabled)
+                {
+                    metricUpdateTimer.Enabled = true;
+                }
+                Thread.Sleep(10); // probing every 10 seconds for timer
+            }
+            Debug.WriteLine("xx");
+            */
+        }
+
+        private void UpdateMetrics(object sender, ElapsedEventArgs e)
+        {
+            Debug.WriteLine("Update metics...");
+            //return;
+            foreach (CounterEntry entry in this.RegisteredCounts)
+            {
+                long rawValue = entry.PerfCounter.RawValue;
+                float nextValue = entry.PerfCounter.NextValue();
+                Debug.WriteLine(String.Format(@"<PerfCounter> {0}\{1}\{2}: {3}", entry.PerfCounter.CategoryName, entry.PerfCounter.CounterName, entry.PerfCounter.InstanceName, nextValue));
+                Prometheus.Gauge g = (Prometheus.Gauge)entry.PrometheusCollector;
+                g.Labels(entry.PerfCounter.InstanceName).Set(nextValue);
+            }
+
+        }
+
+        private void RegisterPrintSpooler()
+        {
+            string categoryName = "Print Queue";
+            PerformanceCounterCategory perfCategory = new PerformanceCounterCategory(categoryName);
+            string[] instanceNames = perfCategory.GetInstanceNames().Where(name => name != "_Total").ToArray();
+            foreach (string instanceName in instanceNames)
+            {
+                foreach (string counterName in new string[] { "Total Jobs Printed", "Jobs", "Job Errors" })
+                {
+                    System.Diagnostics.PerformanceCounter tmpPerfCounter = new System.Diagnostics.PerformanceCounter();
+                    tmpPerfCounter.CategoryName = categoryName;
+                    tmpPerfCounter.CounterName = counterName;
+                    tmpPerfCounter.InstanceName = instanceName;
+
+                    string metricName = string.Format("perfmon_{0}_{1}_total", categoryName.Replace(" ", "_").ToLower().Trim(), counterName.Replace(" ", "_").ToLower().Trim());
+                    Prometheus.Gauge tmpPrometheusCounter = Metrics.CreateGauge(metricName, "help text", labelNames: new[] { "name" });
+
+                    CounterEntry entry = new CounterEntry() { PerfCounter = tmpPerfCounter, PrometheusCollector = tmpPrometheusCounter };
+                    this.RegisteredCounts.Add(entry);
+                }
+
+            }
+
+        }
+    }
+}
