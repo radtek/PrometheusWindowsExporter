371bb8d85bdc28093e34266dac8ae17a6dbd1fb4
tsauter <tsauter@gmx.net>
2016-03-14 10:48:08 +0100

    Start the prometheus server during service startup.

--- NodeExporterWindows.Service/NodeExporterService.cs
+++ NodeExporterWindows.Service/NodeExporterService.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+using PrometheusCollector;
+using Prometheus;
 
 // Docu: http://blog.nager.at/2012/01/visual-studio-2010-windows-service-erstellen/
 // #Install
@@ -26,10 +30,37 @@ namespace NodeExporterWindows.Service
 
         protected override void OnStart(string[] args)
         {
+            string dllDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string[] pluginFiles = Directory.GetFiles(dllDirectory, "PrometheusCollector.*.dll");
+
+            foreach (string filename in pluginFiles)
+            {
+                IPrometheusCollector plugin = this.LoadAssembly(filename);
+                plugin.RegisterMetrics();
+            }
+
+            MetricServer metricServer = new MetricServer(port: 9100);
+            metricServer.Start();
         }
 
         protected override void OnStop()
         {
         }
+
+        private IPrometheusCollector LoadAssembly(string assemblyPath)
+        {
+            string assembly = Path.GetFullPath(assemblyPath);
+            Assembly ptrAssembly = Assembly.LoadFile(assembly);
+            foreach (Type item in ptrAssembly.GetTypes())
+            {
+                if (!item.IsClass) continue;
+                if (item.GetInterfaces().Contains(typeof(IPrometheusCollector)))
+                {
+                    return (IPrometheusCollector)Activator.CreateInstance(item);
+                }
+            }
+            throw new Exception("Invalid DLL, Interface not found!");
+        }
+
     }
 }
