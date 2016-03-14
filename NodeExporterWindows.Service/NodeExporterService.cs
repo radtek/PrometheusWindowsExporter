2cf97176919b85a88678e40213fa2ffe539ea2d7
tsauter <tsauter@gmx.net>
2016-03-14 10:10:04 +0100

    Import basic code for Windows Service.

---
+++ NodeExporterWindows.Service/NodeExporterService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+// Docu: http://blog.nager.at/2012/01/visual-studio-2010-windows-service-erstellen/
+// #Install
+// InstallUtil.exe "C:\Projects\WindowsService1\bin\Debug\WindowsService1.exe"
+// #Uninstall
+// InstallUtil.exe /u "C:\Projects\WindowsService1\bin\Debug\WindowsService1.exe"
+
+
+namespace NodeExporterWindows.Service
+{
+    public partial class NodeExporterService : ServiceBase
+    {
+        public NodeExporterService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
