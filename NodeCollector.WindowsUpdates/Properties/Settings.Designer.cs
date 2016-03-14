1ce6b220bd210825590b59f08bca71236cb2f316
tsauter <tsauter@gmx.net>
2016-03-14 14:16:04 +0100

    Update search interval for Windows Updates.

--- NodeCollector.WindowsUpdates/Properties/Settings.Designer.cs
+++ NodeCollector.WindowsUpdates/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace NodeCollector.WindowsUpdates.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("60")]
+        [global::System.Configuration.DefaultSettingValueAttribute("3600")]
         public int SearchInvervalSeconds {
             get {
                 return ((int)(this["SearchInvervalSeconds"]));
